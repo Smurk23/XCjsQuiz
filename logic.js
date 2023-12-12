7e4f1a8d181ae9b1940b6346486e48f71cc002ec
Smurk23 <xingcantres@gmail.com>
2023-12-12 17:43:15 -0500

    adding more logic

--- logic.js
+++ logic.js
@@ -6,8 +6,23 @@ let btn1 = document.querySelector('#option-1');
 let btn2 = document.querySelector('#option-2');
 let btn3 = document.querySelector('#option-3');
 let btn4 = document.querySelector('#option-4');
-let currentIndex = 0
+let currentIndex = 0;
+let timer = document.querySelector('#time');
+let timeLeft = 60;
+let endDiv = document.querySelector('#end');
 
+function endQuiz() {
+    quizDiv.classList.add('hide')
+    endDiv.classList.remove('hide')
+}
+
+function startTimer () {
+setInterval(function(){
+    timer.textContent = timeLeft
+    timeLeft--
+
+}, 1000)
+}
 
 function renderQuestion () {
     questionh2.textContent = questions[currentIndex].title
@@ -18,15 +33,26 @@ function renderQuestion () {
 
 }
 
-function nextQuestion () {
+function nextQuestion (event) {
+    let selectedAnswer = event.target.textContent
+    let correctAnswer = questions[currentIndex].answer
+
+    if(selectedAnswer !== correctAnswer) {
+        timeLeft-= 10
+    }
+if(currentIndex < 4){
     currentIndex++
     renderQuestion()
+} else {
+    endQuiz()
+}
 }
 
 startBtn.addEventListener('click', function(){
     startDiv.classList.add('hide')
     quizDiv.classList.remove('hide')
 renderQuestion()
+startTimer()
 
 })
 
