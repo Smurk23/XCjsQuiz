8d2d27df0f3420c0143cba0584ac3de42b8a7665
Smurk23 <xingcantres@gmail.com>
2023-12-12 17:26:07 -0500

    adding logic

--- logic.js
+++ logic.js
@@ -1,8 +1,36 @@
 let startBtn = document.querySelector('#startButton');
 let startDiv = document.querySelector('#starting');
 let quizDiv = document.querySelector('#Quiz');
+let questionh2 = document.querySelector('#Question');
+let btn1 = document.querySelector('#option-1');
+let btn2 = document.querySelector('#option-2');
+let btn3 = document.querySelector('#option-3');
+let btn4 = document.querySelector('#option-4');
+let currentIndex = 0
+
+
+function renderQuestion () {
+    questionh2.textContent = questions[currentIndex].title
+    btn1.textContent = questions[currentIndex].choices[0]
+    btn2.textContent = questions[currentIndex].choices[1]
+    btn3.textContent = questions[currentIndex].choices[2]
+    btn4.textContent = questions[currentIndex].choices[3]
+
+}
+
+function nextQuestion () {
+    currentIndex++
+    renderQuestion()
+}
 
 startBtn.addEventListener('click', function(){
     startDiv.classList.add('hide')
     quizDiv.classList.remove('hide')
+renderQuestion()
+
 })
+
+btn1.addEventListener('click', nextQuestion)
+btn2.addEventListener('click', nextQuestion)
+btn3.addEventListener('click', nextQuestion)
+btn4.addEventListener('click', nextQuestion)
